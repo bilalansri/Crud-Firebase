7a29aabd49c384435054dfb7c2c72beeaacde814
bilalansri <100476764+bilalansri@users.noreply.github.com>
2022-08-13 16:10:14 +0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGoivkhtnRX3EiKQwORK9CcqGvYzrvaKk",
-  authDomain: "crud-4ef87.firebaseapp.com",
-  databaseURL: "https://crud-4ef87-default-rtdb.firebaseio.com",
-  projectId: "crud-4ef87",
-  storageBucket: "crud-4ef87.appspot.com",
-  messagingSenderId: "735454464508",
-  appId: "1:735454464508:web:57114ac64cce42c1454a79"
+  apiKey: "Add",
+  authDomain: "Add",
+  databaseURL: "ADd",
+  projectId: "ADd",
+  storageBucket: "Add",
+  messagingSenderId: "Add",
+  appId: "Add"
 };
 
 const app = initializeApp(firebaseConfig);
